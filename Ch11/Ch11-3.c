45a72466dbbb3c7374e290ddb0bf56b1a3b4cfac
Blake Hepner <blakewhepner@gmail.com>
2015-02-23 17:11:56 -0700

    Cleaned up comments of Ch11-3.c

--- Ch11/Ch11-3.c
+++ Ch11/Ch11-3.c
@@ -1,31 +1,5 @@
 #include <stdio.h>
-/*
-int main(void)
-{
-  int top, bottom, m, n, temp;
-  
-  printf("Enter a fraction: ");
-  scanf("%d/%d", &top, &bottom);
-  //The following "for" statement is Euclid's algorithm for calculating GCD (greatest common divisor)
-  
-  m = top;
-  n = bottom;
-  
-  for(;;)
-    {
-      if (n == 0) break;
-      temp = m % n;
-      m = n;
-      n = temp;
-    }
-    
-  top /= m;
-  bottom /= m;
-  
-  printf("In lowest terms: %d/%d\n", top, bottom);
-  return 0;
-}
-*/
+
 void reduce(int, int, int *, int *);
 
 int main(void)

fa005dc8adae41307cf943f95273f739d9d21196
Blake Hepner <blakewhepner@gmail.com>
2015-02-23 17:11:10 -0700

    Completed Ch11-3.c

--- Ch11/Ch11-3.c
+++ Ch11/Ch11-3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int main(void)
 {
   int top, bottom, m, n, temp;
@@ -24,4 +24,37 @@ int main(void)
   
   printf("In lowest terms: %d/%d\n", top, bottom);
   return 0;
+}
+*/
+void reduce(int, int, int *, int *);
+
+int main(void)
+{
+  int numerator, denominator, reduced_numerator, reduced_denominator;
+  
+  printf("Enter a fraction: ");
+  scanf("%d/%d", &numerator, &denominator);
+  
+  reduce(numerator, denominator, &reduced_numerator, &reduced_denominator);
+  
+  printf("In lowest terms: %d/%d\n", reduced_numerator, reduced_denominator);
+  return 0;
+}
+
+void reduce(int numerator, int denominator, int *reduced_numerator, int *reduced_denominator)
+{
+  int temp;
+  *reduced_numerator = numerator;
+  *reduced_denominator = denominator;
+  
+  for(;;)
+  {
+    if(denominator == 0) break;
+    temp = numerator % denominator;
+    numerator = denominator;
+    denominator = temp;
+  }
+  
+  *reduced_numerator /= numerator;
+  *reduced_denominator /= numerator;
 }

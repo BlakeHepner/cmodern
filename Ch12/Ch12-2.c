56db72033e9e962dd1fbcdfa802149f742462f61
Blake Hepner <blakewhepner@gmail.com>
2015-02-25 16:12:15 -0700

    Completed Ch12-2.c, used pointers from the biginning (so much easier) so I say it counts as 12-2a and 12-2b

--- Ch12/Ch12-2.c
+++ Ch12/Ch12-2.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define MAX_CHAR 80
+#define MAX_CHAR 80 //This is the maximum size of the input
 
 int main(void)
 {
-  char *pointer, *reverse;
-  char ch;
-  char input[MAX_CHAR];
+  char *pointer, *reverse, ch, input[MAX_CHAR];
   
-  printf("Enter a message: ");
+  printf("Enter a message (Maximum of %d alpha characters): ", MAX_CHAR);
   for (pointer = input; pointer < input + MAX_CHAR;)
   {
-    if ((ch = getchar()) == '\n') break;
-    if (isalpha(ch))
+    if ((ch = getchar()) == '\n') break; //Exits when encounter end of line.
+    if (isalpha(ch)) //Ensures only alpha characters are stored.
     {
-      *pointer++ = tolower(ch);
+      *pointer = tolower(ch);
+      pointer++;
     }
   }
   
-  
+  pointer--; //This is here because the above for statement sets pointer 1 higher than it should be.
   for (reverse = input; pointer >= input; pointer--, reverse++)
-    if (*reverse == *pointer);
-    else
+  {
+    if (*pointer != *reverse) //Compares letters starting at opposite sides of the array.
     {
       printf("Not a palindrome.\n");
       return 1;
     }
+  }
   
   printf("Palindrome.\n");
   return 0;

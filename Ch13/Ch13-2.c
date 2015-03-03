7041bffbb5ee672b140a1a668f25c6090078fbaa
Blake Hepner <blakewhepner@gmail.com>
2015-03-02 17:16:31 -0700

    Partial completion of Ch13-2.c part b, skipping part c

--- Ch13/Ch13-2.c
+++ Ch13/Ch13-2.c
@@ -10,28 +10,36 @@ int read_line(char str[], int n);
 
 int main(int argc, char *argv[])
 {
-  char reminders[MAX_REMIND][MSG_LEN+3];
-  char day_str[3], msg_str[MSG_LEN+1];
-  int day, i, j, num_remind = 0;
+  char reminders[MAX_REMIND][MSG_LEN+8];
+  char day_str[3], msg_str[MSG_LEN+1], time_str[5];
+  int day, hourmin, i, j, num_remind = 0;
   
   for(;;)
   {
-    if (num_remind == MAX_REMIND)
+    if (num_remind == MAX_REMIND) //Stops user from overflowing array
     {
       printf("-- No space left --\n");
       break;
     }
     
-    printf("Enter day and reminder: ");
-    scanf("%2d", &day);
+    printf("Enter day, time(HHMM) and reminder: ");
+    scanf("%2d %4d", &day, &hourmin);
     if (day == 0)
       break;
-    if (day > 31 || day < 0)
+    else if (day > 31 || day < 0) //Error checking for date field
     {
-      printf("Error! You entered an invalid day of the month, you entered: %d", day);
+      printf("Error! You entered an invalid day of the month. You entered: %d\n", day);
       continue;
     }
+    
+    if (hourmin > 2400 || hourmin < 0) //Error checking for time field
+    {
+      printf("Invalid time! You entered %d. Valid values are 0-2400.\n", hourmin);
+      continue;
+    }
+    
     sprintf(day_str, "%2d", day);
+    sprintf(time_str, "%4d", hourmin);
     read_line(msg_str, MSG_LEN);
     
     for (i = 0; i < num_remind; i++)

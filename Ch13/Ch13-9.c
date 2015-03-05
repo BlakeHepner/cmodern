4d33b985f2b37735efdfa75f1ef7b87a0d21f2c4
Blake Hepner <blakewhepner@gmail.com>
2015-03-04 17:03:36 -0700

    Completed Ch13-9.c

--- Ch13/Ch13-9.c
+++ Ch13/Ch13-9.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#define MAX_INPUT_COUNT 40
+
+int compute_vowel_count(const char *sentence);
+
 int main(void)
 {
-  
-  char ch;
   int vowel = 0;
+  char sentence[MAX_INPUT_COUNT];
   
   printf("Enter a sentence: ");
-  while ( (ch = getchar()) != '\n' )
-    {
-      switch (toupper(ch))
-        {
-          case 'A':
-          case 'E':
-          case 'I':
-          case 'O':
-          case 'U': vowel++; break;
-        }
-    }
+  fgets(sentence, MAX_INPUT_COUNT, stdin);
+  vowel = compute_vowel_count(sentence);
   printf("Your sentence contains %d vowels.\n", vowel);
   return 0;
+}
+
+int compute_vowel_count(const char *sentence)
+{
+  int vowel = 0;
+  for(int i = 0; sentence[i] != '\0'; i++)
+  {
+    switch (toupper(sentence[i]))
+    {
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U': vowel++; break;
+      default: break;
+    }
+  }
+  return vowel;
 }

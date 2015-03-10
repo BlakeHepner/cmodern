81f829de3e67e33b4a190dba49b96291c767f9ad
Blake Hepner <blakewhepner@gmail.com>
2015-03-10 09:52:19 -0700

    Completed Ch13-11.c

--- Ch13/Ch13-11.c
+++ Ch13/Ch13-11.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 
+#define MAX_INPUT_LENGTH 80
+
+double compute_average_word_length(const char *sentence);
+
 int main(void)
 {
-  char ch;
-  int letterc = 0, wordc = 1;
-  float average;
-  
+  double average = 0;
+  char input[MAX_INPUT_LENGTH];
   //ch == current character
   //letterc == letter count
   //wordc == word count
   
   printf("Enter a sentence: ");
+  fgets(input, MAX_INPUT_LENGTH, stdin);
   
-  while((ch = getchar()) != '\n')
-    {
-      if (ch == ' ')
-        wordc++;
-      else
-        letterc++;
-    }
-  
-  average = (float) letterc / (float) wordc;
-  
+  average = compute_average_word_length(input);
+
   printf("Average word length: %.1f\n", average);
   return 0;
   
+}
+
+double compute_average_word_length(const char *sentence)
+{
+  int lettercount = 0, wordcount = 1;
+  double average;
+  
+  for (; *sentence != '\0' && *sentence != '\n'; sentence++)
+  {
+    if (*sentence == ' ')
+      wordcount++;
+    else
+      lettercount++;
+  }
+  
+  average = (double) lettercount / (double) wordcount;
+  
+  return average;
 }

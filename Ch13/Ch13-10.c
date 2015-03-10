4af2b2aa95558c35864f7f34d0b8357b6ea9595a
Blake Hepner <blakewhepner@gmail.com>
2015-03-09 17:40:48 -0700

    Completed Ch13-10.c

--- Ch13/Ch13-10.c
+++ Ch13/Ch13-10.c
@@ -1,18 +1,35 @@
 //Enter a first and last name:
+
+//Hepner, B.
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME_LENGTH 20
+
+void reverse_name(char *name);
 
 int main(void)
 {
-  char firstinitial, lastname;
+  char input[MAX_NAME_LENGTH];
   
   printf("Enter a first and last name: ");
-  while ((firstinitial = getchar()) == ' ');
+  fgets(input, MAX_NAME_LENGTH, stdin);
   
-  while (getchar() != ' ');
+  reverse_name(input);
   
-  while ((lastname = getchar()) != '\n')
-    putchar(lastname);
-  
-  printf(", %c\n", firstinitial);
+  printf("%s", input);
+  printf("\n");
   return 0;
+}
+
+void reverse_name(char *name)
+{
+  char first[MAX_NAME_LENGTH], last[MAX_NAME_LENGTH];
+  
+  sscanf(name, " %s %s ", first, last);
+  
+  strcpy(name, last);
+  strcat(name, ", ");
+  strncat(name, first, 1);
+  strcat(name, ".");
 }

b39eeea56c0b35f6890d9c8709fea8c11ff9ce05
Blake Hepner <blakewhepner@gmail.com>
2015-03-25 16:38:43 -0700

    Corrected a few typos

--- Ch15/Ch15-4/readline.c
+++ Ch15/Ch15-4/readline.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "readline.h"
 
 int read_line(char str[], int n)
--- Ch15/Ch15-4/readline.h
+++ Ch15/Ch15-4/readline.h
@@ -1,4 +1,4 @@
 #ifndef READLINE
 #define READLINE
-#int read_line(char str[], int n);
+int read_line(char str[], int n);
 #endif

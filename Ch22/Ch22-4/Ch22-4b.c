512a33570492bd256fabda10d6fc05cedb038e35
Blake Hepner <blakewhepner@gmail.com>
2015-04-27 15:51:00 -0700

    Completed Ch22-4b

--- Ch22/Ch22-4/Ch22-4b.c
+++ Ch22/Ch22-4/Ch22-4b.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[])
 {
-    int charcount = 0;
+    int wordcount = 0;
     FILE *fp;
+    int ch;
+    int insideword = 0;
 
     if ((fp = fopen(argv[1], "r")) == NULL) {
         printf("Error! Unable to open %s", argv[1]);
         return 1;
     }
 
-    while(getc(fp) != EOF) {
-        charcount++;
+    while((ch = getc(fp)) != EOF) {
+        if (isspace(ch)) {
+            insideword = 0;
+        } else if (insideword == 0) {
+        wordcount++;
+        insideword=1;
+        }
     }
     fclose(fp);
-    printf("Character count (including whitespace): %d\n", charcount);
+    printf("Word count: %d\n", wordcount);
     return 0;
 }

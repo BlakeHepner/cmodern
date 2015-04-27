e69b8ca375fbd923c618e1333498154d62b75728
Blake Hepner <blakewhepner@gmail.com>
2015-04-27 15:56:52 -0700

    Completed Ch22-4c.c

--- Ch22/Ch22-4/Ch22-4c.c
+++ Ch22/Ch22-4/Ch22-4c.c
@@ -2,18 +2,21 @@
 
 int main(int argc, char *argv[])
 {
-    int charcount = 0;
+    int linecount = 0;
     FILE *fp;
+    int ch; //stores characters
 
     if ((fp = fopen(argv[1], "r")) == NULL) {
         printf("Error! Unable to open %s", argv[1]);
         return 1;
     }
 
-    while(getc(fp) != EOF) {
-        charcount++;
+    while((ch = getc(fp)) != EOF) {
+        if (ch == '\n') {
+            linecount++;
+        }
     }
     fclose(fp);
-    printf("Character count (including whitespace): %d\n", charcount);
+    printf("Line count: %d\n", linecount);
     return 0;
 }

bd3f719b0dda115d8f4aac4c913e9082ab807d7a
Blake Hepner <blakewhepner@gmail.com>
2015-04-27 17:02:21 -0700

    Ch22-5.c completed

--- Ch22/Ch22-5/Ch22-5.c
+++ Ch22/Ch22-5/Ch22-5.c
@@ -2,19 +2,31 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 #define KEY '&'
 
-int main(void)
+int main(int argc, char *argv[])
 {
     int orig_char, new_char;
+    FILE *origfp, *encfp;
 
-    while((orig_char = getchar()) != EOF) {
+    if (argc != 3) {
+        printf("Usage: %s sourcefile encryptedfile", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    if ((origfp = fopen(argv[1], "rb")) == NULL) {
+        printf("Error! %s cannot be read!", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    if ((encfp = fopen(argv[2], "wb")) == NULL) {
+        printf("Error! Cannot write to %s.\n", argv[2]);
+        exit(EXIT_FAILURE);
+    }
+
+    while((orig_char = getc(origfp)) != EOF) {
         new_char = orig_char ^ KEY;
-        if (isprint(orig_char) && isprint(new_char))
-            putchar(new_char);
-        else
-            putchar(orig_char);
+        putc(new_char, encfp);
     }
 
     return 0;
